5b9b5c469827f46b0270877e5f8dfc1291827aa5
shokbhl <shokbhl@gmail.com>
2018-10-24 12:56:33 +0400

    Initial Commit

---
+++ gitHubLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubLearning
+//
+//  Created by shok bhl on 10/24/18.
+//  Copyright © 2018 shb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
